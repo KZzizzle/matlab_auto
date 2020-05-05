ed082e09a2b8004e214ebc86d5ad1d6d7577752b
Katie Zhuang <katie.zhi.zhuang@gmail.com>
2020-05-05 18:07:13 +0200

    add files

--- create_cookie.py
+++ create_cookie.py
@@ -2,23 +2,38 @@
 from cookiecutter.main import cookiecutter
 from shutil import copyfile
 import sys
+import json
+from pathlib import Path
 
+def main(sourcepath, servicename):
 
-def main(servicename):
-    file = open("sampledat.json", "r")
-    num_inputs = int(file.read())+3
+    metadata_file = Path(sourcepath+"/sampledat.json")
 
-    cookiecutter('https://github.com/ITISFoundation/cookiecutter-osparc-service', extra_context={
-        'docker_base':"ubuntu:18.04",
-        'number_of_inputs': num_inputs, 
-        'affiliation': 'ITIS Foundation',
-        'project_name': servicename,
-        'author_name': "Katie Zhuang",
-        'author_email': "zhuang@itis.swiss",
-        'project_type': "computational",
-        'number_of_outputs': 1,
-        'git_username': "KZzizzle"
-    })
+    try:
+        with metadata_file.open("r") as fp:
+            metadata_dict = json.load(fp)
+
+        num_inputs = len(metadata_dict['serviceInterface']['inputs'])
+        num_outputs = len(metadata_dict['serviceInterface']['outputs'])
+    except:
+        print('Could not find metadata file in source code folder')
+        return 3
+
+    try:
+        cookiecutter('https://github.com/ITISFoundation/cookiecutter-osparc-service', extra_context={
+            'docker_base':"ubuntu:18.04",
+            'number_of_inputs': num_inputs, 
+            'author_affiliation': 'ITIS Foundation',
+            'project_name': servicename,
+            'author_name': "Katie Zhuang",
+            'author_email': "zhuang@itis.swiss",
+            'project_type': "computational",
+            'number_of_outputs': num_outputs,
+            'git_username': "KZzizzle"
+        })
+    except:
+        print('Could not create the cookie')
+        return 2
 
     copyfile(servicename + "/docker/ubuntu/Dockerfile", servicename + "/docker/ubuntu/Dockerfile_copy")
     copyfile(servicename + "/service.cli/execute.sh", servicename + "/service.cli/execute_copy.sh")
@@ -27,16 +42,16 @@ def main(servicename):
     return 0
 
 def usage():
-    print("Usage: docker run KZzizzle/opencor-load <str> ")
-    print("  where <str> is the name of the model that you want to run. It must be on the input folder.")
+    print("Usage: create_cookie.py <path> <str> ")
+    print("  where <path> is the path to folder containing source code and  <str> is the name of the service you will create.")
 
 
 if __name__ == "__main__":
     args = sys.argv
     args.pop(0)  # Script name.
-    modelpath = "simulation-experiment.sedml"
 
     try:
+        sourcepath = str(args.pop(0))
         servicename = str(args.pop(0))
     except ValueError:
         print('ValueError in inputs')
@@ -47,4 +62,5 @@ if __name__ == "__main__":
         usage()
         sys.exit(2)
 
-    rc = main(servicename)
+    rc = main(sourcepath, servicename)
+    sys.exit(rc)
--- create_matlab_service.sh
+++ create_matlab_service.sh
@@ -10,7 +10,7 @@ pip install cookiecutter
 pip install PyYAML
 
 # create a new service with added context
-python3 create_cookie.py $2
+python3 create_cookie.py $1 $2
 
 # in the new service directory, make the virtual environment and build the cookie
 make -C $2 .venv
@@ -19,59 +19,59 @@ source "$2/.venv/bin/activate"
 make -C $2 build
 
 # copy the model and the python script to run the model into src directory
-cp $1 "$2/src/$2/"
-cp run_model.py "$2/src/$2/"
+cp -R $1 "$2/src/$2/"
+# cp run_model.py "$2/src/$2/"
 
-# edit files in docker and service.cli 
-python3 customize_cookie.py "${1##*/}" $2
-chmod +x "$2/service.cli/execute.sh"
+# # edit files in docker and service.cli 
+# python3 customize_cookie.py "${1##*/}" $2
+# chmod +x "$2/service.cli/execute.sh"
 
-build and run container, copy validation output to validation folder
-make -C $2 build
-make -C $2 up
-cp "$2/.tmp/output/outputs.csv" "$2/validation/output/"
-rm "$2/validation/output/outputs.json"
-rm "$2/docker/ubuntu/Dockerfile_copy"
-rm "$2/metadata/metadata_copy.yml"
-rm "$2/service.cli/execute_copy.sh"
+# build and run container, copy validation output to validation folder
+# make -C $2 build
+# make -C $2 up
+# cp "$2/.tmp/output/outputs.csv" "$2/validation/output/"
+# rm "$2/validation/output/outputs.json"
+# rm "$2/docker/ubuntu/Dockerfile_copy"
+# rm "$2/metadata/metadata_copy.yml"
+# rm "$2/service.cli/execute_copy.sh"
 
-# run unit and integration tests
-make -C $2 tests
-deactivate
-
-
-# # #=============================== ready to push? takes care of CI =======================================
+# # run unit and integration tests
+# make -C $2 tests
+# deactivate
 
 
-read -p "Where is the osparc-services directory cloned locally [../osparc-services/]: " SERVICES_DIR
-SERVICES_DIR=${SERVICES_DIR:-../osparc-services}
-echo $SERVICES_DIR
-read -p "What name should this folder have on the github repository [oc-guytonmodel]: " FOLDER_NAME
-FOLDER_NAME=${FOLDER_NAME:-oc-guytonmodel}
-echo $FOLDER_NAME
-
-echo "About to create new version of your service! Are you sure you want this? "
-select yn in "yes" "no"; do
-    case $yn in
-        yes )
-        source "$2/.venv/bin/activate"
-        pip install bump2version
-        make -C $2 version-service-major
-        make -C $2 build
-        make -C $2 up
-        deactivate
-        break;;
-        no ) 
-        return;; 
-        *) 
-        echo "Answer '1' or '2'" ;;
-    esac
-done
+# # # #=============================== ready to push? takes care of CI =======================================
 
 
-cp "$2/.github/workflows/github-ci.yml" "$2/.github/workflows/github-ci_copy.yml" 
-python3 edit_ciyaml.py $2 ${FOLDER_NAME}
-rm "$2/.github/workflows/github-ci_copy.yml" 
+# read -p "Where is the osparc-services directory cloned locally [../osparc-services/]: " SERVICES_DIR
+# SERVICES_DIR=${SERVICES_DIR:-../osparc-services}
+# echo $SERVICES_DIR
+# read -p "What name should this folder have on the github repository [oc-guytonmodel]: " FOLDER_NAME
+# FOLDER_NAME=${FOLDER_NAME:-oc-guytonmodel}
+# echo $FOLDER_NAME
 
-cp -R "$2/." "$SERVICES_DIR/services/$FOLDER_NAME"
-mv "$SERVICES_DIR/services/$FOLDER_NAME/.github/workflows/github-ci.yml" "$SERVICES_DIR/.github/workflows/$FOLDER_NAME.yml"
+# echo "About to create new version of your service! Are you sure you want this? "
+# select yn in "yes" "no"; do
+#     case $yn in
+#         yes )
+#         source "$2/.venv/bin/activate"
+#         pip install bump2version
+#         make -C $2 version-service-major
+#         make -C $2 build
+#         make -C $2 up
+#         deactivate
+#         break;;
+#         no ) 
+#         return;; 
+#         *) 
+#         echo "Answer '1' or '2'" ;;
+#     esac
+# done
+
+
+# cp "$2/.github/workflows/github-ci.yml" "$2/.github/workflows/github-ci_copy.yml" 
+# python3 edit_ciyaml.py $2 ${FOLDER_NAME}
+# rm "$2/.github/workflows/github-ci_copy.yml" 
+
+# cp -R "$2/." "$SERVICES_DIR/services/$FOLDER_NAME"
+# mv "$SERVICES_DIR/services/$FOLDER_NAME/.github/workflows/github-ci.yml" "$SERVICES_DIR/.github/workflows/$FOLDER_NAME.yml"
