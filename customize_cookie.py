5960009ee1cce9a53b58177347d6547ad5b80c71
Katie Zhuang <katie.zhi.zhuang@gmail.com>
2020-05-14 18:06:10 +0200

    creates a cookie that passes unit and integration tests from sample matlab code

--- customize_cookie.py
+++ customize_cookie.py
@@ -3,7 +3,7 @@ import sys
 import json
 import pprint
 from pathlib import Path
-import shutil
+import os
 
 
 sourcepath = str(sys.argv[1])
@@ -19,6 +19,7 @@ accepted_datatypes = [
     "data:*/*",
     "data:text/*",
     "data:[image/jpeg,image/png]",
+    "data:application/csv",
     "data:application/json",
     "data:application/json;schema=https://my-schema/not/really/schema.json",
     "data:application/vnd.ms-excel",
@@ -45,6 +46,7 @@ with metadata_file.open('r') as fp:
 # replace default INPUT fields in metadata.yml with values from the submitted meetadata
 input_keymap = {}
 metadata_dict['inputs'] = {}
+input_dict={}
 
 for i in range(num_inputs):    
 
@@ -69,14 +71,23 @@ for i in range(num_inputs):
     }
 
     # fill in file mapping if necessary
-    if datatype == 'data:*/*':
+    if datatype == 'data:*/*' or 'data:application' in datatype:
         answer = ''
         while answer != 'y':
-            inputfile_name = input("\nPlease enter the filename for " + pprint.pformat(model_inputs[i]) + "\nhere : ")
+            inputfile_name = input("\nPlease enter the filename for " + pprint.pformat(model_inputs[i]) 
+            + "\nhere [" + model_inputs[i]['defaultValue'] + "]: ") or model_inputs[i]['defaultValue']
             answer = input("Validation input file for " + model_inputs[i]['label'] + " will be '" + inputfile_name + "' ok? (y/n): ") 
             metadata_dict['inputs'][keyname].update({'fileToKeyMap': {inputfile_name : model_inputs[i]["name"] }})
-        shutil.move(project_name + "/src/" + project_name + "/" + answer, project_name + "/validation/input" + answer)
-
+        try:
+            os.replace(project_name + "/src/" + project_name + "/" + inputfile_name, project_name + "/validation/input/" + inputfile_name)
+        except:
+            print("File cannot be moved to validation folder")
+            quit()
+    elif datatype == 'number':
+        default_number = float(metadata_dict["inputs"][keyname]["defaultValue"])
+        input_dict.update({keyname: default_number})
+    else:
+        input_dict.update({keyname: metadata_dict["inputs"][keyname]["defaultValue"]})
 
 # replace default OUTPUT fields in metadata.yml with values from the submitted meetadata
 output_keymap = {}
@@ -99,25 +110,38 @@ for i in range(num_outputs):
         'displayOrder': i+1,
         'label': model_outputs[i]['label'],
         'description': model_outputs[i]['description'],
-        'type': datatype,
+        'type': datatype
     }
 
     # fill in file mapping if necessary
-    if datatype == 'data:*/*':
+    if datatype == 'data:*/*' or 'data:application' in datatype:
         answer = ''
         while answer != 'y':
-            outputfile_name = input("\nPlease enter the filename for " + pprint.pformat(model_outputs[i]) + "\nhere : ")
+            outputfile_name = input("\nPlease enter the filename for " + pprint.pformat(model_outputs[i]) 
+            + "\nhere [" + model_outputs[i]['validationFile'] + "]: ") or model_outputs[i]['validationFile']
             answer = input("Validation output file for " + model_outputs[i]['label'] + " will be '" + outputfile_name + "' ok? (y/n): ") 
             metadata_dict['outputs'][keyname].update({'fileToKeyMap': {outputfile_name : model_outputs[i]["name"] }})
-        shutil.move(project_name + "/src/" + project_name + "/" + answer, project_name + "/validation/output" + answer)
-
+        try:
+            os.replace(project_name + "/src/" + project_name + "/" + outputfile_name, project_name + "/validation/output/" + outputfile_name)
+        except:
+            print("file cannot be moved to validation folder")
+            quit()
 
 # write to metadata file
 metadata_file_edited = Path(project_name+'/metadata/metadata.yml')
 with metadata_file_edited.open('w') as fp:
     yaml.safe_dump(metadata_dict, fp, default_style=None, default_flow_style=False)
 
-
+#=======================================================================
+# write validation input file for non-file inputs
+#=======================================================================
+inputvalidation_file = Path(project_name+"/validation/input/inputs.json")
+if len(input_dict)>0: 
+    with inputvalidation_file.open("w") as fpin:
+        json.dump(input_dict, fpin, indent=4)
+else:
+    if os.path.exists(inputvalidation_file):
+        os.remove(inputvalidation_file)
 
 #=======================================================================
 # edit the Dockerfile
@@ -138,6 +162,8 @@ WORKDIR /redist
 RUN unzip MATLAB_Runtime_R2019b_Update_4_glnxa64.zip \
  && ./install -v -mode silent -agreeToLicense yes
 RUN chown ${SC_USER_NAME}:${SC_USER_NAME} /usr/local/MATLAB/MATLAB_Runtime/v97/*
+
+WORKDIR /home/${SC_USER_NAME}
 RUN rm -rf /redist
 
 """
@@ -150,10 +176,12 @@ with Docker_fileout.open('w') as dout_file:
     for line in buf:
         # install unzip
         if line .__contains__("jq \\"):
-            line = line + "\tunzip \\\n"
+            line = line + "\tunzip  xorg \\\n"
         # copy Matlab Runtime into image
         elif line .__contains__("copy docker bootup scripts"):
             line = addtext + line
+        elif line .__contains__("RUN cp -R "): 
+            line = "RUN cp -R src/" + project_name + "/* /build/bin\n\n"
         dummyvar = dout_file.write(line)
 
 
@@ -170,24 +198,23 @@ while answer != 'y':
     input_list = [x.strip() for x in input_string.split(',')]
     answer = input("Ordered list of inputs will be  " + str(input_list) + "  ok? (y/n): ") 
 
+executetext =("\necho \"Starting simulation...\"\n/home/scu/" + project_name + "/run_" 
++ project_name + ".sh /usr/local/MATLAB/MATLAB_Runtime/v97/ " 
++ '} '.join("${{{0}".format(x.upper()) for x in input_list) + '}' 
++ "\necho \"Simulation finished!\"\n\ncp *.csv $OUTPUT_FOLDER/\ncp *.mat $OUTPUT_FOLDER/")
 
-executetext = ('\n/home/opencor/OpenCOR-2019-06-11-Linux/bin/OpenCOR -c PythonRunScript::script /home/' 
-+ project_name + '/run_model.py ${INPUT_FOLDER}/input.json /home/' 
-+ project_name + '/' + model_file + ' /home/' + project_name + '/input_keymap.json'
-+ '\n\ncp outputs.csv ${OUTPUT_FOLDER}/outputs.csv\n\nenv | grep INPUT')
 
+with execute_file.open('r') as e_file:
+    ebuf = e_file.readlines()
 
-# with execute_file.open('r') as e_file:
-#     ebuf = e_file.readlines()
-
-# lastline = len(ebuf)
-# with execute_fileout.open('w') as eout_file:
-#     for index, line in enumerate(ebuf):
-#         if line .__contains__('# For example: input_1 -> $INPUT_1'):
-#             line = line + executetext
-#             lastline = index
-#         if index <= lastline:
-#             dummyvar = eout_file.write(line)
+lastline = len(ebuf)
+with execute_fileout.open('w') as eout_file:
+    for index, line in enumerate(ebuf):
+        if line .__contains__('# For example: input_1 -> $INPUT_1'):
+            line = line + executetext
+            lastline = index
+        if index <= lastline:
+            dummyvar = eout_file.write(line)
 
 # #=======================================================================
 # # edit README.md
